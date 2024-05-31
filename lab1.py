2221fb07325a5f2411b26d5db2d38107e70953d9
Goober118 <140124210+Goober118@users.noreply.github.com>
2024-05-31 00:38:55 +0000

    balls

--- lab1.py
+++ lab1.py
@@ -11,40 +11,41 @@
 # Step 1: Define the Item class with initialisation that uses setters for name, price, and quantity.
 # Instead of directly setting private attributes in the __init__ method, use the class's own setters
 # We will define the setters in later steps to add validation to the setting of these attributes.
-
-
-
-
-
+class Item:
+    def __init__(self, name, price, quantity):
+        self._name = name
+        self._price = price
+        self._quantity = quantity
+        
 # Step 2: Implement a getter for the name attribute.
 # This method should simply return the value of the private _name attribute.
-
-
-
-
+    
+    def get_name(self):
+        return self._name
 
 # Step 3: Implement a setter for the name attribute.
 # This method should check if the provided value is a string before setting the _name attribute.
 # If the value is not a string, it should raise a ValueError.
 
-
-
-
+    def set_name(self, new_name):
+        if new_name is "":
+            raise ValueError("Name must not be empty")
+        self._name = new_name
 
 # Step 4: Implement a getter for the price attribute.
 # This method should return the price formatted as a string with two decimal places.
 
-
-
-
+    def get_price(self):
+        return f"{self._name:.2f}"
 
 # Step 5: Implement a setter for the price attribute.
 # This method should check if the provided value is a non-negative number before setting the _price attribute.
 # If the value is negative, it should raise a ValueError.
 
-
-
-
+    def set_price(self, new_price):
+        if new_price < 0:
+            raise ValueError("Price must be a positive number.")
+        self._price = new_price
 
 # Step 6: Implement a getter for the quantity attribute.
 # This method should simply return the value of the private _quantity attribute.
